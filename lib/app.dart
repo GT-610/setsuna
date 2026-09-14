f5691ceb185eaa2a8a4a344c430b1297ccf85fd3
GT610 <myddz1005@163.com>
2026-09-14 15:32:06 +0800

    Restore eased navigation transitions

--- lib/app.dart
+++ lib/app.dart
@@ -775,7 +775,7 @@ class _MainWindowState extends State<MainWindow> with WindowListener, Loggable {
       await _pageController.animateToPage(
         0,
         duration: const Duration(milliseconds: 677),
-        curve: Curves.linear,
+        curve: Curves.fastLinearToSlowEaseIn,
       );
     }
 
@@ -969,7 +969,7 @@ class _MainWindowState extends State<MainWindow> with WindowListener, Loggable {
     _pageController.animateToPage(
       index,
       duration: const Duration(milliseconds: 677),
-      curve: Curves.linear,
+      curve: Curves.fastLinearToSlowEaseIn,
     );
   }
 
